d360d2a419e2749194e183df3fd5869a1a918a55
Michael van Dyk <msvandyk@hotmail.com>
2017-05-06 15:42:59 -0400

    RootishArrayStack made, seems to be very slow

--- Kotlin/kstructs/RootishArrayStack.kt
+++ Kotlin/kstructs/RootishArrayStack.kt
@@ -14,27 +14,57 @@ import kstructs.list.array.ArrayStack
  * @author Michael van Dyk
  */
 class RootishArrayStack<E>: List<E> {
-    private val blocks: List<Array<E>> = ArrayStack()
+
+    private val blocks: List<Array<Any?>> = ArrayStack()
     private var size: Int = 0
 
     private fun i2b(i: Int): Int = Math.ceil((-3.0 + Math.sqrt(9.0 + i * 8.0)) / 2.0).toInt()
 
     override fun add(index: Int, item: E) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val r = blocks.size()
+        if (r * (r + 1) / 2 < size + 1) grow()
+        ++size
+        for (j in (size - 1) downTo (index + 1))
+            set(j, get(j - 1))
+        set(index, item)
     }
 
     override fun get(index: Int): E {
         val b = i2b(index)
         val j = index - b * (b + 1) / 2
-        return (blocks[b][j])
+        @Suppress("UNCHECKED_CAST")
+        return (blocks[b][j] as E)
+    }
+
+    private fun grow() {
+        blocks.add(arrayOfNulls(blocks.size() + 1))
     }
 
     override fun remove(index: Int): E {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val x = get(index)
+        for (j in index until (size - 1))
+            set(j, get(j + 1))
+        --size
+        val r = blocks.size()
+        if ((r - 2) * (r - 1)/2 >= size) shrink()
+        return (x)
     }
 
     override fun set(index: Int, item: E): E {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val b = i2b(index)
+        val j = index - b * (b + 1) / 2
+        @Suppress("UNCHECKED_CAST")
+        val y = blocks[b][j] as E
+        blocks[b][j] = item
+        return (y)
+    }
+
+    private fun shrink() {
+        var r = blocks.size()
+        while (r > 0 && (r - 2) * (r - 1)/2 >= size) {
+            blocks.removeLast()
+            --r
+        }
     }
 
     override fun size(): Int = size
