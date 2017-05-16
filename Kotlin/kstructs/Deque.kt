09c9a5098f1696e93babc7b0d290d6516fdc4c3a
Michael van Dyk <msvandyk@hotmail.com>
2017-05-15 20:19:57 -0400

    Some commenting done

--- Kotlin/kstructs/Deque.kt
+++ Kotlin/kstructs/Deque.kt
@@ -11,6 +11,11 @@ package kstructs
  * @author Michael van Dyk
  */
 interface Deque<E> : Queue<E>, Stack<E>  {
+
+    /**
+     *  Adds [item] to the front of the deque.
+     * @param item the element to add to the collection.
+     */
     fun addFirst(item: E)
     fun getFirst(): E
     fun removeFirst(): E
@@ -18,6 +23,9 @@ interface Deque<E> : Queue<E>, Stack<E>  {
     fun getLast(): E
     fun removeLast(): E
 
+    /**
+     * @inherit
+     */
     override fun add(item: E) = addLast(item)
     override fun get() = getFirst()
     override fun remove() = removeFirst()
